fbd1a0ff1a9fb00168bea20a065f686013250989
Bruna Scauri <brunascauri@hotmail.com>
2023-01-17 15:52:44 -0300

    feat: create new book page validations and it updates the database

---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
